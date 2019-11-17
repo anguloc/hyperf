afd2a00c90c8cdf0035cc0765e3c8d080ce46417
gk <xy62201793@163.com>
2019-11-17 16:28:08 +0800

    push

--- config/autoload/databases.php
+++ config/autoload/databases.php
@@ -13,12 +13,12 @@ declare(strict_types=1);
 return [
     'default' => [
         'driver' => env('DB_DRIVER', 'mysql'),
-        'host' => env('DB_HOST', 'localhost'),
-        'database' => env('DB_DATABASE', 'hyperf'),
-        'username' => env('DB_USERNAME', 'root'),
-        'password' => env('DB_PASSWORD', ''),
-        'charset' => env('DB_CHARSET', 'utf8'),
-        'collation' => env('DB_COLLATION', 'utf8_unicode_ci'),
+        'host' => env('DB_HOST', defined('HOST_1') ? HOST_1 : 'localhost'),
+        'database' => env('DB_DATABASE', 'gk'),
+        'username' => env('DB_USERNAME', defined('USER_1') ? USER_1 : 'root'),
+        'password' => env('DB_PASSWORD', defined('PWD_1') ? PWD_1 : ''),
+        'charset' => env('DB_CHARSET', 'utf8mb4'),
+        'collation' => env('DB_COLLATION', 'utf8mb4_general_ci'),
         'prefix' => env('DB_PREFIX', ''),
         'pool' => [
             'min_connections' => 1,
@@ -26,7 +26,7 @@ return [
             'connect_timeout' => 10.0,
             'wait_timeout' => 3.0,
             'heartbeat' => -1,
-            'max_idle_time' => (float) env('DB_MAX_IDLE_TIME', 60),
+            'max_idle_time' => (float)env('DB_MAX_IDLE_TIME', 60),
         ],
         'cache' => [
             'handler' => Hyperf\ModelCache\Handler\RedisHandler::class,
