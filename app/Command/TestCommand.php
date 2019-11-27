80e3b63c7b056d907043e727270c79e46b0c1c26
张瑜 <xy62201793@163.com>
2019-11-27 21:31:09 +0800

    if

--- app/Command/TestCommand.php
+++ app/Command/TestCommand.php
@@ -7,6 +7,9 @@ namespace App\Command;
 use Hyperf\Command\Command as HyperfCommand;
 use Hyperf\Command\Annotation\Command;
 use Psr\Container\ContainerInterface;
+use xingwenge\canal_php\CanalConnectorFactory;
+use xingwenge\canal_php\CanalClient;
+use xingwenge\canal_php\Fmt;
 
 /**
  * @Command
@@ -32,7 +35,29 @@ class TestCommand extends HyperfCommand
 
     public function handle()
     {
+        try {
+            $client = CanalConnectorFactory::createClient(CanalClient::TYPE_SOCKET_CLUE);
+            # $client = CanalConnectorFactory::createClient(CanalClient::TYPE_SWOOLE);
 
+            $client->connect('www.gkfk5.cn', 11111);
+            $client->checkValid();
+            $client->subscribe("1001", "example", ".*\\..*");
+            # $client->subscribe("1001", "example", "db_name.tb_name"); # 设置过滤
+
+            while (true) {
+                $message = $client->get(100);
+                if ($entries = $message->getEntries()) {
+                    foreach ($entries as $entry) {
+                        Fmt::println($entry);
+                    }
+                }
+                sleep(1);
+            }
+
+            $client->disConnect();
+        } catch (\Exception $e) {
+            echo $e->getMessage(), PHP_EOL;
+        }
 //        $this->line('Hello Hyperf!', 'info');
     }
 }
