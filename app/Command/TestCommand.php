baaa0b3591e4fbfe51efdf2d1d8f9e52a5bdf26d
gk <xy62201793@163.com>
2019-12-08 23:27:49 +0800

    push

--- app/Command/TestCommand.php
+++ app/Command/TestCommand.php
@@ -6,7 +6,10 @@ namespace App\Command;
 
 use Hyperf\Command\Command as HyperfCommand;
 use Hyperf\Command\Annotation\Command;
+use Hyperf\Di\Annotation\Inject;
+use Hyperf\Logger\LoggerFactory;
 use Psr\Container\ContainerInterface;
+use Swoole\Process;
 use xingwenge\canal_php\CanalConnectorFactory;
 use xingwenge\canal_php\CanalClient;
 use xingwenge\canal_php\Fmt;
@@ -21,11 +24,22 @@ class TestCommand extends HyperfCommand
      */
     protected $container;
 
-    public function __construct(ContainerInterface $container)
+    /**
+     * @var \Hyperf\Logger\Logger
+     */
+    protected static $logger;
+
+    protected $masterPid;
+    protected $masterData = [];
+
+    public function __construct(ContainerInterface $container, LoggerFactory $logger)
     {
         $this->container = $container;
         parent::__construct();
         $this->setName("test");
+
+        // log
+        self::$logger = $logger->get('hy-process');
     }
 
     public function configure()
@@ -35,6 +49,24 @@ class TestCommand extends HyperfCommand
 
     public function handle()
     {
+        Process::daemon();
+        self::setProcessName("Hy-Process-Master");
+
+        $this->masterPid = getmypid();
+        $this->masterData['start_time'] = time();
+
+        Process::signal(SIGUSR1, function ($sig) {
+            self::log('Master-USR1');
+        });
+        Process::signal(SIGUSR2, function ($sig) {
+            self::log('Master-USR2');
+        });
+
+
+
+
+
+        return '';
         try {
             $client = CanalConnectorFactory::createClient(CanalClient::TYPE_SOCKET_CLUE);
             # $client = CanalConnectorFactory::createClient(CanalClient::TYPE_SWOOLE);
@@ -59,5 +91,20 @@ class TestCommand extends HyperfCommand
             echo $e->getMessage(), PHP_EOL;
         }
 //        $this->line('Hello Hyperf!', 'info');
+    }
+
+    protected static function setProcessName($name)
+    {
+        if (PHP_OS != 'Darwin' && function_exists('swoole_set_process_name')) {
+            swoole_set_process_name($name);
+        }
+    }
+
+    protected static function log($msg, array $context)
+    {
+        if (is_array($msg) || is_object($msg)) {
+            $msg = json_encode($msg);
+        }
+        self::$logger->info($msg, $context);
     }
 }
