f19f2e538cab61f3a4f5dc4c545d940313f68487
gk <xy62201793@163.com>
2020-12-02 03:46:39 +0800

    启动shell 修改

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-nohup php bin/hyperf.php start &>/var/www/html/hyperf/runtime/logs/run.log 2>&1 &
+nohup php -d swoole.use_shortname=Off bin/hyperf.php start &>/var/www/html/hyperf/runtime/logs/run.log 2>&1 &
